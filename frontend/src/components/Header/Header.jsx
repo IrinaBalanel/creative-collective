48d18868813bf0d0349b33c88cc0ac5e55f05363
Irina Balanel <irina.cowork@gmail.com>
2024-10-15 21:36:33 -0400

    Fixed header's dropdown

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -43,37 +43,31 @@ export default function Header(){
                     <span className="bar"></span>
                 </button>
                 <ul className={`nav-list ${menuVisible ? "show" : ""}`} role="menu">
-                    <Link to="/" role="menulink"><li className="nav-item">
-                        Home
-                    </li></Link>
-                    <Link className="btn-dropdown">
-                        <li className="nav-item dropdown">
-                            Professionals <i aria-label="dropdown button" className="bi bi-caret-down-fill" role="dropdown"></i>
-                            <div className="dropdown-content">
-                                <Link to="/professionals" role="menulink">All Professionals</Link>
-                                {
-                                    categories.map((category) => (
-                                        <Link to={`/professionals/${category.category}`} key={category._id} role="menulink">
-                                            {capitalizeFirstLetter(category.category)}
-                                        </Link>
-                                    ))
-                                }
-                            </div>
-                        </li>
-                    </Link>
-                    <Link smooth to="/#contact" role="menulink">
-                        <li className="nav-item">
-                        Contact
-                        </li>
-                    </Link>
+                    <li className="nav-item">
+                        <Link to="/" role="menulink">Home</Link>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <Link className="btn-dropdown">Professionals <i aria-label="dropdown button" className="bi bi-caret-down-fill" role="dropdown"></i></Link>
+                        <div className="dropdown-content">
+                            <Link to="/professionals" role="menulink">All Professionals</Link>
+                            {
+                                categories.map((category) => (
+                                    <Link to={`/professionals/${category.category}`} key={category._id} role="menulink">
+                                        {capitalizeFirstLetter(category.category)}
+                                    </Link>
+                                ))
+                            }
+                        </div>
+                    </li>
+                    <li className="nav-item">
+                        <Link smooth to="/#contact" role="menulink">Contact</Link>
+                    </li>
 
                     {user && user.role === "client" && (
                         <>
-                            <Link to={`/profile/${user._id}`} role="menulink">
-                                <li className="nav-item">
-                                    My Favorites
-                                </li>
-                            </Link>
+                            <li className="nav-item">
+                                <Link to={`/profile/${user._id}`} role="menulink">My Favorites</Link>
+                            </li>
                             {/* <li className="nav-item logout-small-menu">
                                 
                             </li> */}
@@ -83,63 +77,7 @@ export default function Header(){
                     )}
                     {user && user.role === "provider" && (
                         <>
-                            <Link to="/admin/login" role="menulink">
-                                <li className="nav-item">
-                                    Admin
-                                </li>
-                            </Link>
-                            <Link to="/login" state={{ fromProvider: true }} role="menulink">
-                                <li className="nav-item provider-access-link">
-                                    Already a Provider?
-                                </li>
-                            </Link>
-                            <Link to="/login" className="btn-light" role="menulink"> 
-                                <li className="nav-item">
-                                    Login
-                                </li>
-                            </Link>
-                        </>
-                        
-                    )}
-                    {user && user.role === "admin" && (
-                        <>
-                            <Link to="/admin/login" role="menulink">
-                                <li className="nav-item">
-                                    Admin
-                                </li>
-                            </Link>
-                            <Link to="/login" state={{ fromProvider: true }} role="menulink">
-                                <li className="nav-item provider-access-link">
-                                    Already a Provider?
-                                </li>
-                            </Link>
-                            <Link to="/login" className="btn-light" role="menulink"> 
-                                <li className="nav-item">
-                                    Login
-                                </li>
-                            </Link>
-                        </>
-                        
-                    )}
-
-                    {!user && (
-                        <>
-                            <Link to="/admin/login" role="menulink">
-                                <li className="nav-item">
-                                    Admin
-                                </li>
-                            </Link>
-                            <Link to="/login" state={{ fromProvider: true }} role="menulink">
-                                <li className="nav-item provider-access-link">
-                                    Already a Provider?
-                                </li>
-                            </Link>
-                            <Link to="/login" className="btn-light" role="menulink"> 
-                                <li className="nav-item">
-                                    Login
-                                </li>
-                            </Link>
-                            {/* <li className="nav-item">
+                            <li className="nav-item">
                                 <Link to="/admin/login" role="menulink">Admin</Link>
                             </li>
                             <li className="nav-item provider-access-link">
@@ -147,7 +85,36 @@ export default function Header(){
                             </li>
                             <li className="nav-item">
                                 <Link to="/login" className="btn-light" role="menulink">Login</Link>
-                            </li> */}
+                            </li>
+                        </>
+                        
+                    )}
+                    {user && user.role === "admin" && (
+                        <>
+                            <li className="nav-item">
+                                <Link to="/admin/login" role="menulink">Admin</Link>
+                            </li>
+                            <li className="nav-item provider-access-link">
+                                <Link to="/login" state={{ fromProvider: true }} role="menulink">Already a Provider?</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/login" className="btn-light" role="menulink">Login</Link>
+                            </li>
+                        </>
+                        
+                    )}
+
+                    {!user && (
+                        <>
+                            <li className="nav-item">
+                                <Link to="/admin/login" role="menulink">Admin</Link>
+                            </li>
+                            <li className="nav-item provider-access-link">
+                                <Link to="/login" state={{ fromProvider: true }} role="menulink">Already a Provider?</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/login" className="btn-light" role="menulink">Login</Link>
+                            </li>
                         </>
                         
                     )}
